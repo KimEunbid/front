c5d2194a37bfa99a310cb58789699673a0cbca87
KimEunbid <hwanglover3@snu.ac.kr>
2023-09-25 20:17:51 +0900

    make page 1 2 3

---
+++ src/app/[word]/[field]/page.tsx
@@ -0,0 +1,9 @@
+import data from '../../../../public/data.json'
+
+export default function fieldpage(){
+    return(
+        <div>
+            <h1>{data.description.naturescience.contents}</h1>
+        </div>
+    )
+}
